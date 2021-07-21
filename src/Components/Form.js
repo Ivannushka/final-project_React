a2f6911eaa3af20503df6af01383ab3549e35594
Ivannushka <ivannaorlov@gmail.com>
2021-07-21 12:24:11 +0300

    Update Form.js

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -5,51 +5,58 @@ function Form({someText}) {
     const [text, setText] = useState(someText);
     const [password, setPassword] = useState(someText);
     const [passwordRepeat, setPasswordRepeat] = useState(someText);
+    const [firstName, setFirstName] = useState(someText);
+    const [secondName, setSecondName] = useState(someText);
+
     const addText = () => {
-    //     if (text == '') {
-    //         alert('No name passed');
-    //         return;
-    //     } else if (password == '') {
-    //         alert("Please, enter your password")
-    //         return;
-    //     } else if  (passwordRepeat !== password) {
-    //         alert ('Passwords do not mach!')
-    //         return;
-    //     }
-    // else if  (password.length < 9) {
-    //     alert ('Password should contain not less then 9 symbols')
-    //     return false;
-    // }
-    // else {
-    //     alert('Form has been sucessfully submitted')
-    // }
+        if (text == '') {
+            alert('No username passed');
+            return;
+        } else if (password == '') {
+            alert("Please, enter your password")
+            return;
+        } else if  (passwordRepeat !== password) {
+            alert ('Passwords do not mach!')
+            return;
+        }
+    else if  (password.length < 9) {
+        alert ('Password should contain not less then 9 symbols')
+        return;
+    }
+    else if (firstName == '') {
+        alert('Please, enter your first name')
+        return;
+    }
+    else {
+        alert('Form has been sucessfully submitted')
+    }
  
             setText('');
         setPassword('');
         setPasswordRepeat('');
-    
+    setFirstName('');
+    setSecondName('');
     }
   
-let alertMessage = <h6></h6>
-if (text == '') {
-    return (<h6 style={{color:'red'}}>Username must be filled</h6>
-
-    )
+// let alertMessage = <h6></h6>
+// if (text == '') {
+//     return (<h6 style={{color:'red'}}>Username must be filled</h6>
+//     )
 //     let alertMessage = <h6></h6>
 // if (text == '') {alertMessage = <h6 style={{color:'red'}}>Username must be filled</h6>
     
-} else if (password == '') {alertMessage = <h6 style={{color:'red'}}>Please, enter your paswword</h6>
-}  
-else if (password < 9){ alertMessage = <h6 style={{color:'red'}}>Password should contain not less then 9 symbols</h6>
+// } 
+// else if (password == '') {alertMessage = <h6 style={{color:'red'}}>Please, enter your paswword</h6>
+// }  
+// else if (password < 9){ alertMessage = <h6 style={{color:'red'}}>Password should contain not less then 9 symbols</h6>
+// }
+
+// else if (passwordRepeat !== password){ alertMessage = <h6 style={{color:'red'}}>Password do not match</h6>
+// }
+
+const updateText = (event) => {
+    setText(event.target.value);
 }
-
-else if (passwordRepeat !== password){ alertMessage = <h6 style={{color:'red'}}>Password do not match</h6>
-}
-
-// for (let i=0; i<=9; i++) { alertMessage.push(<h6 style={{color:'red'}}>not less then 9 symbolds</h6>)}
-// for (let i=0; i<=9; i++) { alertMessage = <h6 style={{color:'red'}}>not less then 9 symbolds</h6>}
-// else if (password <9) {alertMessage = <h6 style={{color:'red'}}>not less then 9 symbolds</h6>}
-
     const updatePassword  = (event) => {
         setPassword(event.target.value);
     }
@@ -57,39 +64,13 @@ else if (passwordRepeat !== password){ alertMessage = <h6 style={{color:'red'}}>
         setPasswordRepeat(event.target.value);
     }
 
-    const updateText = (event) => {
-        setText(event.target.value);
-
-        // updateCallback(event.target.value);
-
-  
-        
-    // }
+const updateFirstName = (event) => {
+    setFirstName(event.target.value)
 }
-    //     if (updateText === "");
-    //  console.log ('this field must be filled')}
-        
-// EXAMPLE
-    // import React from "react";
-    // import { useForm } from "react-hook-form";
-    
-    // export default function App() {
-    //   const { register, formState: { errors }, handleSubmit } = useForm();
-      
-    //   return (
-    //     <form onSubmit={handleSubmit(onSubmit)}>
-    //       <input {...register("firstName", { required: true })} />
-    //       {errors.firstName?.type === 'required' && "First name is required"}
-          
-    //       <input {...register("lastName", { required: true })} />
-    //       {errors.lastName && "Last name is required"}
-          
-    //       <input type="submit" />
-    //     </form>
-    //   );
-    // }
-    
-    
+const updateSecondName = (event) => {
+    setSecondName(event.target.value)
+}
+
 
     return(
         <div className="row">
@@ -97,7 +78,7 @@ else if (passwordRepeat !== password){ alertMessage = <h6 style={{color:'red'}}>
         <h5>Register</h5>
       {/* <form onsubmit="validateForm(event)" id="registerForm" className="formWithValidation" action="" name="forma" > */}
       <div>
-          {alertMessage}
+          {/* {alertMessage} */}
         <input type="text" id="register-username" value=""  className="field" placeholder= "Username*" name="user" value={text} onChange={updateText}/>
       </div>
 <div>
@@ -107,10 +88,10 @@ else if (passwordRepeat !== password){ alertMessage = <h6 style={{color:'red'}}>
 <input type="password" id="retype-password"  className="field" value="" placeholder= "Retype password*" name="passRepeat" value={passwordRepeat} onChange={updatePasswordRepeat}/>
 </div>
 <div>
-<input type="text" id="last-name"  className="field" value="" placeholder= "First name*" required value={text} onChange={updateText} />
+<input type="text" id="last-name"  className="field" value="" placeholder= "First name*" required value={firstName} onChange={updateFirstName} />
 </div>
 <div>       
-<input type="text" id="last-name" className="field" value="" placeholder= "Last name" value={text} onChange={updateText}/>
+<input type="text" id="last-name" className="field" value="" placeholder= "Last name" value={secondName} onChange={updateSecondName}/>
 </div>
              
 <div>
